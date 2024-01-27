87fde4150a0523994924c59aa4112fdae90b0038
TastedPotential <113959158+TastedPotential@users.noreply.github.com>
2024-01-27 16:45:50 -0500

    Added a new custom font and mainText display. The beginings of the text scrambler have begun. I'm first showing the baseline text with each letter rendered as a individual character for easier manipulation. Also testing out a new monospace font.

--- TextChar.js
+++ TextChar.js
@@ -1,19 +1,23 @@
-function TextChar(inputChar){
-    this.savedChar = inputChar;
-    this.isLocked = false;
-    this.relativePos = -1;
-    this.familyOrder = -1;
-    this.familyID = -1;
-    this.familySize = 0;
+class TextChar{
 
-    this.isShaking = false;
-    this.animStartTime = 0;
-    this.animDurationMillis = 333;
-    this.animDurationNanos = this.animDurationMillis * 1000000;
-
-    this.distanceFromLockCenter = 0;
-    this.shakeDistanceMax = 0;
-    this.shakeDirection = 1;
-    this.shakeSpeed = windowWidth / 30;
+    constructor(inputChar){
+        this.savedChar = inputChar;
+        this.isLocked = false;
+        this.relativePos = -1;
+        this.familyOrder = -1;
+        this.familyID = -1;
+        this.familySize = 0;
+    
+        this.isShaking = false;
+        this.animStartTime = 0;
+        this.animDurationMillis = 333;
+        this.animDurationNanos = this.animDurationMillis * 1000000;
+    
+        this.distanceFromLockCenter = 0;
+        this.shakeDistanceMax = 0;
+        this.shakeDirection = 1;
+        this.shakeSpeed = windowWidth / 30;
+    
+    }
 
 }
--- sketch.js
+++ sketch.js
@@ -1,28 +1,47 @@
-var charsAL = [];
-var allCharString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+let charsArray = [];
+let textAnchorX;
+let textAnchorY;
+let sizeOfText;
+let widthOfText;
+let textGap;
+var sketchFont;
+
+function preload(){
+  sketchFont = loadFont('SpaceMono-Regular.ttf');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  textAnchorX = windowWidth/2;
+  textAnchorY = windowHeight/2;
+  sizeOfText = windowWidth/15;
+  widthOfText = sizeOfText/2;
+  textGap = sizeOfText / 9; // divide by 20 for default monospace, 9 for SpaceMono.
 
-  print("allCharString length: " + allCharString.length);
-  print("random index from allCharString: " + floor(random(0, allCharString.length)));
-  print("Random float: " + Math.floor(Math.random() * (allCharString.length)));
+  textFont(sketchFont);
+  textSize(sizeOfText);
 
-  for( var i = 0; i < 63; i++){
-    let randIndex = Math.floor(Math.random() * (allCharString.length));
-    let randChar = allCharString.charAt(randIndex);
-    print("randomIndex was: " + randIndex + " with char: " + randChar + " " + Number(random()).toFixed(2));
-    charsAL[i] = new TextChar(randChar);
+  let startingString = "InitialScramble";
+  for(let i = 0; i < startingString.length; i++){
+    charsArray.push(new TextChar(startingString.charAt(i)));
   }
-
   
 }
 
 function draw() {
   background(220);
-  textSize(windowWidth/15);
-  textAlign(CENTER);
-  text("This site will contain \nmy project.", windowWidth / 2, windowHeight * .48);
+  textSize(sizeOfText);
+  //textAlign(CENTER);
+  for(let i = 0; i < charsArray.length; i++){
+
+    // This centers the main text horizontally on the screen.
+    let displayTextXOffset = ((charsArray.length * widthOfText) / 2) + (((charsArray.length - 1) * textGap) / 2);
+
+    text(charsArray[i].savedChar, textAnchorX - displayTextXOffset + (i * widthOfText) + i * textGap, textAnchorY);
+  }
+
+  // let displayTextXOffset = (charsArray.length * (widthOfText) / 2);
+  // text("InitialScramble", textAnchorX - displayTextXOffset, textAnchorY + 2 * sizeOfText);
 }
 
 function windowResized(){
