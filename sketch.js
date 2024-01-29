32f81a4200615d983499a1386d7964916767db04
TastedPotential <113959158+TastedPotential@users.noreply.github.com>
2024-01-29 15:36:28 -0500

    Changed font again to SometypeMono. I'm still not 100% setteld on a font, but this font is evenly spaced and sized, but doesn't have the wonky occasional charcaters like r, y, n and more from Ubuntu.

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var sketchFont;
 function preload(){
   // Went with Ubuntu Mono since it's one of the most equally spaced fonts I found.
   // From Google Fonts: https://fonts.google.com/specimen/Ubuntu+Mono?query=ubuntu&classification=Monospace
-  sketchFont = loadFont('UbuntuMono-Regular.ttf'); 
+  sketchFont = loadFont("fonts/SometypeMono-Regular.ttf");
 }
 
 function setup() {
@@ -18,7 +18,7 @@ function setup() {
   textAnchorY = windowHeight/2;
   sizeOfText = windowWidth/15;
   widthOfText = sizeOfText/2;
-  textGap = 0; // divide by 20 for default monospace, 9 for SpaceMono, 0 value for Ubuntu (not needed)
+  textGap = 0; // divide by 20 for default monospace, 9 for SpaceMono, 0 value for Sometype (not needed)
 
   textFont(sketchFont);
   textSize(sizeOfText);
@@ -33,19 +33,24 @@ function setup() {
 function draw() {
   background(220);
   textSize(sizeOfText);
-  //textAlign(CENTER);
-  for(let i = 0; i < charsArray.length; i++){
-
-    // This centers the main text horizontally on the screen.
-    let displayTextXOffset = ((charsArray.length * widthOfText) / 2) + (((charsArray.length - 1) * textGap) / 2);
-
-    text(charsArray[i].savedChar, textAnchorX - displayTextXOffset + (i * widthOfText) + i * textGap, textAnchorY);
-  }
-
-  // let displayTextXOffset = (charsArray.length * (widthOfText) / 2);
-  // text("InitialScramble", textAnchorX - displayTextXOffset, textAnchorY + 2 * sizeOfText);
+  textFont(sketchFont);
+  drawText(0, 0);
 }
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function drawText(yOffset, kerning){
+  if(kerning == 0)
+    textGap = kerning;  
+  else
+    textGap = widthOfText / kerning;
+
+  // This centers the main text horizontally on the screen.
+  let windowCenterTextOffsetX = ((charsArray.length * widthOfText) / 2) + (((charsArray.length - 1) * textGap) / 2);
+
+  for(let i = 0; i < charsArray.length; i++){
+    text(charsArray[i].savedChar, textAnchorX - windowCenterTextOffsetX + (i * widthOfText) + i * textGap, textAnchorY + yOffset);
+  }
 }
